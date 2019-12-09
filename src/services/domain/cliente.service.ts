a841bd8ff425fbc8c86d2e93ae119b7a2ea98aae
dimas-flj <dimasflj@gmail.com>
2019-12-09 12:16:22 -0300

    Interceptor para incluir token nas requisições

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,24 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
-import { StorageService } from "../storage.service";
 
 @Injectable()
 export class ClienteService {
-	constructor(
-		public http: HttpClient,
-		public storage: StorageService
-	) { }
+	constructor(public http: HttpClient) { }
 
 	findByEmail(email: string): Observable<ClienteDTO> {
-		let token = this.storage.getLocalUser().token;
-		let authHeader = new HttpHeaders({ "Authorization": "Bearer " + token });
-
 		return this.http.get<ClienteDTO>(
-			`${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-			{ "headers": authHeader }
+			`${API_CONFIG.baseUrl}/clientes/email?value=${email}`
 		);
 	}
 
