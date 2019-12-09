dd1366b58fafb1a3ad6a1755125e822191f1a5ec
dimas-flj <dimasflj@gmail.com>
2019-12-09 13:28:02 -0300

    Fazendo o app aproveitar o usuário logado na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,40 +5,47 @@ import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component(
-    {
-        selector: 'page-home',
-        templateUrl: 'home.html'
-    }
+	{
+		selector: 'page-home',
+		templateUrl: 'home.html'
+	}
 )
 
 export class HomePage {
-    creds : CredenciaisDTO = {
-        email: "",
-        senha: ""
-    };
+	creds: CredenciaisDTO = {
+		email: "",
+		senha: ""
+	};
 
-    constructor(
-        public navCtrl: NavController,
-        public menu: MenuController,
-        public auth: AuthService
-    ) {};
+	constructor(
+		public navCtrl: NavController,
+		public menu: MenuController,
+		public auth: AuthService
+	) { };
 
-    ionViewWillEnter() {
-        this.menu.swipeEnable(false);
-    };
+	ionViewDidEnter() {
+		this.auth.refreshToken()
+			.subscribe(
+				response => {
+					this.auth.successfulLogin(response.headers.get('Authorization'));
+					this.navCtrl.setRoot('CategoriasPage');
+				},
+				error => { }
+			);
+	};
 
-    ionViewDidLeave() {
-        this.menu.swipeEnable(true);
-    };
-    
-    login() {
-        this.auth.authenticate(this.creds)
-            .subscribe(
-                response => {
-                    this.auth.successfulLogin(response.headers.get('Authorization'));
-                    this.navCtrl.setRoot('CategoriasPage');
-                },
-                error => {}
-            );
-    };
+	ionViewDidLeave() {
+		this.menu.swipeEnable(true);
+	};
+
+	login() {
+		this.auth.authenticate(this.creds)
+			.subscribe(
+				response => {
+					this.auth.successfulLogin(response.headers.get('Authorization'));
+					this.navCtrl.setRoot('CategoriasPage');
+				},
+				error => { }
+			);
+	};
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -8,32 +8,43 @@ import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
-    jwtHelper: JwtHelper = new JwtHelper();
+	jwtHelper: JwtHelper = new JwtHelper();
 
-    constructor(public http: HttpClient, public storage: StorageService) {}
+	constructor(public http: HttpClient, public storage: StorageService) { }
 
-    authenticate(creds : CredenciaisDTO) {
-        return this.http.post(
-            `${API_CONFIG.baseUrl}/login`,
-            creds,
-            {
-                observe: 'response',
-                responseType: 'text'
-            }
-        );
-    }
+	authenticate(creds: CredenciaisDTO) {
+		return this.http.post(
+			`${API_CONFIG.baseUrl}/login`,
+			creds,
+			{
+				observe: 'response',
+				responseType: 'text'
+			}
+		);
+	}
 
-    successfulLogin(authorizationValue : string) {
-        let tok = authorizationValue.substring(7);
-        let user : LocalUser = {
-            token: tok,
-            email: this.jwtHelper.decodeToken(tok).sub
-        };
+	refreshToken() {
+		return this.http.post(
+			`${API_CONFIG.baseUrl}/auth/refresh_token`,
+			{},
+			{
+				observe: 'response',
+				responseType: 'text'
+			}
+		);
+	}
 
-        this.storage.setLocalUser(user);
-    }
+	successfulLogin(authorizationValue: string) {
+		let tok = authorizationValue.substring(7);
+		let user: LocalUser = {
+			token: tok,
+			email: this.jwtHelper.decodeToken(tok).sub
+		};
 
-    logout() {
-        this.storage.setLocalUser(null);
-    }
+		this.storage.setLocalUser(user);
+	}
+
+	logout() {
+		this.storage.setLocalUser(null);
+	}
 }
